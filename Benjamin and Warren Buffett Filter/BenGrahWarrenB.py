cd7fa35f8d8c6d034b86d631027fc287f53e4822
Ketan <ramanetiketan.work@gmail.com>
2022-09-25 01:56:07 +0530

    Rectified scraping errors due to terminology change in the websites.

--- Benjamin and Warren Buffett Filter/BenGrahWarrenB.py
+++ Benjamin and Warren Buffett Filter/BenGrahWarrenB.py
@@ -1,11 +1,77 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Benjamin Graham and Warren Buffett Model Stock Exchange
+
+# ## Introduction:
+# * There are about 4000 stocks which are actively traded on the stock exchanges at BSE and NSE.
+# * We can extract public financial data from sites like to find which are the fundamentally strong stocks. 
+# * On what stocks would the father of value investing, Benjamin Graham and Warren Buffett the most successful investors in the world make their investments on.
+
+# ## Benjamin Graham and Warren Buffett Model:
+# 1. **Step 1:** Filtering out all companies with sales less than Rs 250 cr. Companies with sales lower than this are very small companies and might not have the business stability and access to finance that is required for a safe investment. This eliminates the basic business risk.
+# 2. **Step 2:** Filtering out all companies with debt to equity greater than 30%. Companies with low leverage are safer.
+# 3. **Step 3:** Filtering out all companies with interest coverage ratio of less than 4. Companies with high interest coverage ratio have a highly reduced bankruptcy risk.
+# 4. **Step 4:** Filtering out all companies with ROE less than 15% since they are earning less than their cost of capital. High ROE companies have a robust business model, which generates increased earnings for the company typically.
+# 5. **Step 5:** Filtering out all companies with PE ratio greater than 25 since they are too expensive even for a high-quality company. This enables us to pick companies which are relatively cheaper as against their actual value. He points out that applying these filters enables us to reduce and even eliminate a lot of fundamental risks while ensuring a robust business model, strong earning potential and a good buying price.
+
+# ## Implementation of the Model.
+
+# In[1]:
+
+
+# importing required libraries
+
 import requests
 import re
 from bs4 import BeautifulSoup
 import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+# Read the data from the csv files
+
 data=pd.read_csv("Companies.csv")
+data.head()
+
+
+# ### Analysing the dataset / Data cleaning.
+
+# In[3]:
+
+
+# Checking for null values
+
+data.isnull().sum()
+
+
+# In[4]:
+
+
+data.info()
+
+
+# In[5]:
+
+
+data.describe()
+
+
+# * The data looks clean and ready to be used.
+
+# ### Extracting the links of the companies from the data.
+# 
+# **Note:** The data is being extracted from [Money Control Website](https://www.moneycontrol.com/)
+
+# In[6]:
+
+
 companynames=list(data.iloc[:,0])
 links=list(data.iloc[:,1])
-import numpy as np
+
 #links for old format page
 linkso=[]
 for i in range(0,len(links)):
@@ -19,16 +85,45 @@ for i in range(0,len(linkso)):
     a=linkso[i].find('balance-sheet')
     b=linkso[i][:a]+'profit-loss'+linkso[i][a+13:]
     linkso_pl.append(b)
-    
-''' Scrapping Net Sales'''
+
+
+# In[7]:
+
+
+linkso
+
+
+# In[8]:
+
+
+linkso_pl
+
+
+# ## Implementation of Step - 1 :
+# **Filtering out all companies with sales less than Rs 250 cr.**
+
+# ### Scrapping Net Sales
+# <br>
+# 
+# **Note:** 
+# * As the details of net sales was not provided, the following banks were excluded from the file:
+# > HDFC bank, ICICI bank, Indusland bank, Kotak Mahindra Bank, Yes Bank, SBI
+# * As interestt cover ratio was not provided, the following were also excluded.
+# > Bajaj Finserv and Infosys 
+# 
+
+# In[9]:
+
 
 netsales_list=[]
-for i in range(0,len(linkso_pl)):
-    pageold=requests.get(linkso_pl[i])
+for i in linkso_pl:
+    i_ind = i.find('profit-loss')
+    i = i[:i_ind]+'results/yearly'+i[i_ind+11:]
+    pageold=requests.get(i)
     soup = BeautifulSoup(pageold.text, 'html.parser')
     about=soup.findAll('table')
     about=str(about)
-    a=re.findall('<td>Net Sales</td>\n.*</td>',about)
+    a=re.findall('<td>Net Sales/Income from operations</td>\n.*</td>',about)
     netsales=re.findall('>[-+]?[0-9].*<',str(a))
     netsales=netsales[0][1:len(netsales[0])-1]
     if(netsales.find(',') != -1):
@@ -37,9 +132,23 @@ for i in range(0,len(linkso_pl)):
         b=-1
     else:
         netsales_list.append(float(netsales))
-        
-'''  The Net Sales of each company is saved in the netsales list in Crores.
-     Now filtering out the companies with less than 250 cr. Net Sales.'''
+
+
+# * The Net Sales of each company is saved in the netsales list in Crores.
+
+# In[10]:
+
+
+# values in cr.
+netsales_list
+
+
+# #### Filtering. 
+# * The companies with less than 250 cr. Net Sales are to be filtered out/removed.
+
+# In[11]:
+
+
 filtersales=[]
 filtercomp=[]
 filterlink=[]
@@ -57,7 +166,16 @@ for i in range(0,len(filtersales)):
     linkso.remove(filterolink[i])
 
 
-''' Scraping debt equity Ratio '''
+# ## Implementatin of Step - 2 :
+# **Filtering out all companies with debt to equity greater than 30%.**
+
+# ### Scraping debt equity Ratio
+
+# * **Some of the data to be extracted is in old page format.**
+# * **The links of the old page is stored in linkso_r list.**
+
+# In[12]:
+
 
 #links for ratio page
 linkso_r=[]
@@ -67,12 +185,12 @@ for i in range(0,len(linkso)):
     linkso_r.append(b)
     
 debtequity_ratio=[]
-for i in range(0,len(linkso_r)):
-    pageold=requests.get(linkso_r[i])
+for i in linkso_r:
+    pageold=requests.get(i)
     soup = BeautifulSoup(pageold.text, 'html.parser')
     about=soup.findAll('table')
     about=str(about)
-    a=re.findall('<td>Debt Equity Ratio</td>\n.*</td>',about)
+    a=re.findall('<td>Total Debt/Equity.*</td>\n.*</td>',about)
     der=re.findall('>[-+]?[0-9].*<',str(a))
     if(len(der)==0):
         der=0
@@ -80,7 +198,17 @@ for i in range(0,len(linkso_r)):
         der=der[0][1:len(der[0])-1]
     debtequity_ratio.append(float(der))
 
-#Companies with Debt Equity Ratio greater than 0.3 are filtered 
+
+# In[13]:
+
+
+debtequity_ratio
+
+
+# #### Companies with Debt Equity Ratio greater than 0.3 are filtered/removed.
+
+# In[14]:
+
 
 filtersales=[]  
 filterder=[]
@@ -102,7 +230,14 @@ for i in range(0,len(filterder)):
     netsales_list.remove(filtersales[i])
 
 
-''' Scraping Interest Coverage Ratio '''
+# ## Implementation of Step - 3 :
+# **Filtering out all companies with interest coverage ratio of less than 4.**
+# <br>
+# ### Scraping Interest Coverage Ratio.
+
+# In[15]:
+
+
 #links for ratio page
 linkso_r=[]
 
@@ -113,12 +248,14 @@ for i in range(0,len(linkso)):
 
 icr_list=[]
 
-for i in range(0,len(linkso_r)): 
-    pageold=requests.get(linkso_r[i])
+for i in linkso_r: 
+    ind_ = i.find('ratios')
+    i = i[:ind_]+'consolidated-'+i[ind_:]
+    pageold=requests.get(i)
     soup = BeautifulSoup(pageold.text, 'html.parser')
     about=soup.findAll('table')
     about=str(about)
-    a=re.findall('<td>Interest Cover</td>\n.*</td>',about)
+    a=re.findall('<td>Interest Coverage Ratios ...</td>\n.*</td>',about)
     #icr = re.findall("[^a-zA-Z:]([-+]?\d+[\.]?\d*)", str(a))
     icr=re.findall('>[-+]?[0-9].*<',str(a))
     icr=icr[0][1:len(icr[0])-1]
@@ -128,6 +265,30 @@ for i in range(0,len(linkso_r)):
         b=-1
     else:
         icr_list.append(float(icr))
+
+
+# In[16]:
+
+
+icr
+
+
+# In[17]:
+
+
+about
+
+
+# In[18]:
+
+
+icr_list
+
+
+# #### Filtering out all companies with interest coverage ratio of less than 4.
+
+# In[19]:
+
 
 filtericr=[]
 filtercomp=[]
@@ -150,9 +311,22 @@ for i in range(0,len(filtericr)):
     linkso.remove(filterolink[i])   
     netsales_list.remove(filtersales[i])
     debtequity_ratio.remove(filterder[i])
-    
-    
-''' Scraping Total Income'''
+
+
+# ## Implementation of Step - 4 :
+# **Filtering out all companies with ROE less than 15%**
+
+# * ROE stands for **Return on Equity.**
+# * ROE is calculated by using the formula showm below.
+
+# ![roe-return-on-equity.png](attachment:roe-return-on-equity.png)
+
+# * Net Income can be scrapped directly from the website.
+# 
+# **Scraping Total Income**
+
+# In[20]:
+
 
 # links for profit loss page is stored in links_pl
 links_pl=[]
@@ -178,7 +352,23 @@ for i in range(0,len(links_pl)):
         ti_list.append(float(ti))
 
 
-''' Scraping Equity Share Capital'''
+# * Total Net Income has been scrapped and stored in the ti_list variable.
+
+# In[21]:
+
+
+ti_list
+
+
+# **Scraping Shareholder's Equity Share Capital**
+
+# * Total assests can be extracted directly from the website.
+# * Total Liabilities is **Total Non-Current Liabilities + Total Current Liabilities.**
+# * Total Non-Current Liabilities and Total Current Liabilities can be extracted from the website and later used in the formula.
+
+# In[22]:
+
+
 tot_assets=[]
 current_lia=[]
 noncurrent_lia=[]
@@ -225,13 +415,63 @@ for i in range(0,len(links)):
     else:
         current_lia.append(float(totcl))
     
+
+
+# * The Total Assets is stored in tot_assets variable.
+
+# In[23]:
+
+
+tot_assets
+
+
+# * Total Non-Current Liabilities is stored in noncurrent_lia variable
+
+# In[24]:
+
+
+noncurrent_lia
+
+
+# * Total Current Liabilities is stored in current_lia variable.
+
+# In[25]:
+
+
+current_lia
+
+
+# **Calculating ROE from the scrapped data.**
+
+# In[26]:
+
+
 # calculating Shareholder's Equity
 
-share_equity = np.array(tot_assets)-(np.array(current_lia) + np.array(noncurrent_lia))    
+share_equity = np.array(tot_assets)-(np.array(current_lia) + np.array(noncurrent_lia))
+
+
+# In[27]:
+
+
 roe_list = np.array(ti_list)/share_equity 
 roe_list = roe_list.tolist()
 
-# Filtering ROE values less than 0.15
+
+# In[28]:
+
+
+# Values are in proportion.
+roe_list
+
+
+# ### Filtering ROE values.
+# 
+# * Filtering companies having ROE value less than 15% (0.15).
+
+# In[29]:
+
+
 filtersales=[]  
 filterder=[]
 filtericr=[]
@@ -257,7 +497,21 @@ for i in range(0,len(filterroe)):
     links.remove(filterlink[i])
 
 
-''' Scraping for PE'''
+# ## Implementation of Step - 5 :
+# **Filtering out all companies with PE ratio greater than 25 since they are too expensive even for a high-quality company**
+
+# * PE stands for **Price per Earning Ratio.**
+# * PE can be calculated by using the formula shown below.
+
+# ![PE_Ratio.png](attachment:PE_Ratio.png)
+
+# * Current Share price is not available directly from the website.
+# * It can be calculated using the formula **Price per book value * Book value per Share.**
+
+# ### Scraping for Current Share Price.
+
+# In[30]:
+
 
 # links for ratio
 
@@ -301,6 +555,28 @@ for i in range(0,len(links_r)):
     else:
         bkvshr.append(float(pbvs))
 
+
+# * Price per book value is stored in the pricbv variable.
+
+# In[31]:
+
+
+pricbv
+
+
+# * Book-value per share is store in bkvshr variable.
+
+# In[32]:
+
+
+bkvshr
+
+
+# ### Scraping Earnings Per Share.
+
+# In[33]:
+
+
 eps_list=[]
 for i in range(0,len(links_pl)):
     pageold=requests.get(links_pl[i])
@@ -317,10 +593,38 @@ for i in range(0,len(links_pl)):
     else:
         eps_list.append(float(eps))
 
+
+# * Earnings per share is stored in eps_list variable.
+
+# In[34]:
+
+
+eps_list
+
+
+# ### Calculating Price to Equity Ratio (PE) as per the formula.
+
+# In[35]:
+
+
 pe=np.array(bkvshr)*np.array(pricbv)/np.array(eps_list)
 pe=pe.tolist()
 
-''' Filter PE values '''
+
+# * PE ratio values are store in pe variable.
+
+# In[36]:
+
+
+pe
+
+
+# **Filtering out all companies with PE ratio greater than 25**
+
+# In[37]:
+
+
+''' Filtering wrt PE values '''
 filtersales=[]  
 filterder=[]
 filtericr=[]
@@ -347,13 +651,35 @@ for i in range(0,len(filterpe)):
     pe.remove(filterpe[i])
     companynames.remove(filtercomp[i])
     links.remove(filterlink[i])
-    
+
+
+# ## Results:
+# * The Benjamin Graham and Warren Buffer filter was implemented on the given companies to filter out the comapanies which are poor in the current stock market.
+
+# **The final list of companies that have passed the filter is made into a dataframe.**
+
+# In[38]:
+
 
 final=list(zip(companynames,netsales_list,debtequity_ratio,icr_list,roe_list,pe))
 
 filtered_list=pd.DataFrame(final,columns=['Company','Net Sales in cr.','Debt to Equity Ratio','Interest Coverage Ratio','Return On Equity (ROE)','P/E Ratio'])
 
-#Saving post filteration to csv file
-filtered_list.to_csv('After_Filteration.csv', index=False)
+
+# In[39]:
+
 
 print(filtered_list)
+
+
+# * Storing these companies in an excel sheet.
+
+# In[40]:
+
+
+filtered_list.to_csv('After_Filteration.csv', index=False)
+
+
+# ## Note:
+# * These values and filteration process is dynamic.
+# * The data and values might change based on the company's performance in the stock market.
